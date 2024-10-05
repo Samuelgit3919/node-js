1fef5aa2af19a48e61dbf406885f25361c3b4fd9
samuelgit <saminew3919@gmail.com>
2024-10-05 06:17:50 -0400

    commit

--- middle-ware/solution3.js
+++ middle-ware/solution3.js
@@ -4,7 +4,7 @@ const app = express();
 const port = 3000;
 
 function logger(req, res, next) {
-  console.log("Request Method: ", req.method);
+  console.log("Request : ", req.method);
   console.log("Request URL: ", req.url);
   next();
 }
